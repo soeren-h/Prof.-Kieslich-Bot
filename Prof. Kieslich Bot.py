e9371162eef44552d437a7fafbe9bc18cd5307b6
soeren-h <soerenses49@gmail.com>
2021-06-11 18:14:59 +0200

    Update:
    Social Commands

--- Prof. Kieslich Bot.py
+++ Prof. Kieslich Bot.py
@@ -3,10 +3,12 @@ import random
 
 bot = discord.Client()
 
+
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game('Wartungsarbeiten!'))
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game('mit Chemikalien'))
     print('Booting complete')
+
 
 @bot.event
 async def on_message(message):
@@ -16,7 +18,6 @@ async def on_message(message):
     memes = ['https://cdn.discordapp.com/attachments/800792706833973289/838155560519336021/unknown.png',
             'https://cdn.discordapp.com/attachments/808418244628840499/838396797461463050/Frederik2_meme.png',
             'https://cdn.discordapp.com/attachments/808418244628840499/838396800490536960/Frederik9_meme.png',
-            #'https://cdn.discordapp.com/attachments/808418244628840499/838396800523829309/Frederik8_meme.png',
             'https://cdn.discordapp.com/attachments/808418244628840499/838396804164485170/Frederik12_meme.png',
             'https://cdn.discordapp.com/attachments/808418244628840499/838398668285280266/Frederik7_meme.png',
             'https://cdn.discordapp.com/attachments/808418244628840499/838398670662533160/Frederik13_meme.png',
@@ -25,7 +26,8 @@ async def on_message(message):
             'https://cdn.discordapp.com/attachments/808418244628840499/838398917501648916/Frederik10_meme.png',
             'https://cdn.discordapp.com/attachments/808418244628840499/838399397205246022/Frederik3_meme.png',
             'https://cdn.discordapp.com/attachments/808418244628840499/838400124023209994/Frederik15_meme.png', 
-            'https://cdn.discordapp.com/attachments/808418244628840499/838400508045033512/Frederik18_meme.png']
+            'https://cdn.discordapp.com/attachments/808418244628840499/838400508045033512/Frederik18_meme.png',
+            'https://cdn.discordapp.com/attachments/800792706833973289/840338275926409226/unknown.png']
 
     explosion = ['https://cdn.discordapp.com/attachments/833098898930139229/838392942107951174/unknown.png',
                  'https://cdn.discordapp.com/attachments/833098898930139229/838393529772408892/unknown.png',
@@ -48,9 +50,12 @@ async def on_message(message):
         embed.add_field(name='-meme', value='Sendet ein zufälliges Meme von Frederik.', inline=False)
         embed.add_field(name='-special', value='Mit diesem Befehl könnt ihr Auswählen, welches Abonnenten Special euch geschickt werden soll.', inline=False)
         embed.add_field(name='-explosion', value='Sendet ein zufälliges Bild von einer Explosion.', inline=False)
+        embed.add_field(name='-socials <parameter>', value='Sendet den jeweiligen Link social Media Link von Frederik. Gültige Parameter sind: **yt** um sich den Link von seinem YouTube Kanal zu schicken, **i** für Instagram und **a** für alle Links. Der jeweilige Befehl muss ohne die Klammern angegeben werden!')
         embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/833098898930139229/838490284241191002/Logoentwurf2neuer.jpg')
         embed.set_footer(icon_url='https://cdn.discordapp.com/attachments/808418244628840499/839113433727107102/Profilbild.png' ,text='Programmiert von Sören#2416')
         await message.channel.send(embed=embed)
+
+        #await message.channel.send('**Dieser Befehl ist aufgrund von Wartungsarbeiten temporär nicht verfügbar!**')
 
     if message.content.lower() == '-special':
         
@@ -85,49 +90,54 @@ async def on_message(message):
     if message.content.lower() == '-explosion':
         await message.channel.send(random.choice(explosion))
 
+    if message.content.lower() == '-git':
+        await message.channel.send('https://github.com/soeren-h/Prof.-Kieslich-Bot')
+
     if message.content.lower().startswith('-socials'):
         
-        b = ['yt', 'i', 'a', 'youtube', 'y', 'instagram', 'insta', 'all']
+        b = ['yt', 'youtube' 'y', 'i', 'instagram', 'insta', 'a', 'all']
 
         txt = message.content
         input = txt.split(' ')
 
         if len(input) == 2:
             
+            count = 0
+
             tmp = "".join(input[1])
-            
-            for word in b:
-                if word in message.content.lower():
 
-                    if tmp.lower() == 'yt' or tmp.lower() == 'youtube' or tmp.lower() == 'y':
-                        await message.channel.send('https://www.youtube.com/channel/UC5E-DDOyl_CLi5BCbFHv2vQ')
-                        break
+            if tmp.lower() == 'yt' or tmp.lower() == 'youtube' or tmp.lower() == 'y':
+                await message.channel.send('https://www.youtube.com/channel/UC5E-DDOyl_CLi5BCbFHv2vQ')
 
-                    elif tmp.lower() == 'i' or tmp.lower() == 'instagram' or tmp.lower() == 'insta':
+            elif tmp.lower() == 'i' or tmp.lower() == 'instagram' or tmp.lower() == 'insta':
                         
-                        embed = discord.Embed(color=0xbc2a8d)
+                embed = discord.Embed(color=0xbc2a8d)
 
-                        embed.add_field(name='https://www.instagram.com/prof.kieslichs_chem_lab/?hl=de', value='Mein Instagram')
+                embed.add_field(name='https://www.instagram.com/prof.kieslichs_chem_lab/?hl=de', value='Mein Instagram')
 
-                        await message.channel.send(embed=embed)
-                        break
+                await message.channel.send(embed=embed)
 
-                    elif tmp.lower() == 'a' or tmp.lower() == 'all':
-                        await message.channel.send('**Hier mein YouTube:**')
-                        await message.channel.send('https://www.youtube.com/channel/UC5E-DDOyl_CLi5BCbFHv2vQ')
+            elif tmp.lower() == 'a' or tmp.lower() == 'all':
+                await message.channel.send('**Hier mein YouTube:**')
+                await message.channel.send('https://www.youtube.com/channel/UC5E-DDOyl_CLi5BCbFHv2vQ')
 
-                        embed = discord.Embed(color=0xbc2a8d)
+                embed = discord.Embed(color=0xbc2a8d)
 
-                        embed.add_field(name='https://www.instagram.com/prof.kieslichs_chem_lab/?hl=de', value='Mein Instagram')
+                embed.add_field(name='https://www.instagram.com/prof.kieslichs_chem_lab/?hl=de', value='Mein Instagram')
 
-                        await message.channel.send('**Hier mein Instagram:**')
-                        await message.channel.send(embed=embed)
-                        break
-                else:
-                    await message.channel.send('**Error!** Kein gültiger Parameter angegeben! Wenn du nicht weiter weißt schau bei **-help** oder wende dich an einen **Admin** oder **Moderator**.')
+                await message.channel.send('**Hier mein Instagram:**')
+                await message.channel.send(embed=embed)
+            else:
+                await message.channel.send('**Error!** Kein gültiger Parameter angegeben! Wenn du nicht weiter weißt schau bei **-help** oder wende dich an einen **Admin** oder **Moderator**.')
 
         else:
-            await message.channel.send('**Error!** Kein Parameter angegeben! Wenn du nicht weiter weißt schau bei **-help** oder wende dich an einen **Admin** oder **Moderator**.')    
+            await message.channel.send('**Error!** Kein Parameter angegeben! Wenn du nicht weiter weißt schau bei **-help** oder wende dich an einen **Admin** oder **Moderator**.')
 
+    mention = '414055162613727243'
+    if mention in message.content:
+        emoji = bot.get_emoji(838092798125277184)
+
+        print('dumm')
+        await message.add_reaction(str(emoji))
 
 bot.run('')
